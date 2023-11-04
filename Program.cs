c9271debda749dc82041886459d39a4bbe2d27e3
ryosyntaxx <riodwi12174@gmail.com>
2023-11-04 14:04:17 +0700

    fist commit

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main()
+    {
+        
+        string url = "https://ngl.link/api/submit";
+
+        
+        using (HttpClient client = new HttpClient())
+        {
+
+            FormUrlEncodedContent formData = new FormUrlEncodedContent(new[]
+            {
+                new KeyValuePair<string, string>("username", "ryosoraa"),
+                new KeyValuePair<string, string>("question", "helooo"),
+                new KeyValuePair<string, string>("deviceId", "f317828d-2aaf-48c4-aa3c-4f1b7deae77e"),
+                new KeyValuePair<string, string>("gameSlug", ""),
+                new KeyValuePair<string, string>("referrer", "")
+            });
+
+            
+
+            for(int i = 1; i < 4; i++)
+            {
+            HttpResponseMessage response = await client.PostAsync(url, formData);
+                
+                if (response.IsSuccessStatusCode)
+                {
+                    
+                    string responseContent = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine("Response: " + responseContent);
+                }
+                else
+                {
+                    Console.WriteLine("HTTP POST request gagal dengan kode status: " + response.StatusCode);
+                }
+
+            }
+
+
+        }
+    }
+}
---
+++ src/menu/Interfaces.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NGL_Spammer.NET.src.menu
+{
+    internal class Interfaces
+    {
+        public void choise()
+        {
+
+            ArrayList choices = new ArrayList();
+            
+
+            Console.WriteLine("==================================");
+            Console.WriteLine("+     Insert UserName Target     +");
+            Console.WriteLine("==================================");
+            Console.Write("->");
+            choices.Add(Console.ReadLine());
+
+            Console.WriteLine("==================================");
+            Console.WriteLine("+          Insert Type           +");
+            Console.WriteLine("==================================");
+            Console.Write("->");
+            choices.Add(Console.ReadLine());
+
+        }
+    }
+}
